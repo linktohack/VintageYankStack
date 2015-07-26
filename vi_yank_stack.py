1ad7e300b367b0cd79e7d1d25cea18bf2306ad1d
Quang-Linh LE <linktohack@gmail.com>
2015-07-26 19:40:11 +0200

    It works!

--- vi_yank_stack.py
+++ vi_yank_stack.py
@@ -1,22 +1,46 @@
+from pprint import pprint
+
 import sublime, sublime_plugin
 from Vintage.vintage import g_registers, clip_empty_selection_to_line_contents
 
 debug = lambda *args, **kwargs: None
 debug = print
 
-def run_fallback_command(view, command, scope, args):
-    if command:
-        if scope == 'window':
-            view.window().run_command(command, args)
-        elif scope == 'view':
-            view.run_command(command, args)
+def last_command_before_yank_stack(view):
+    i = 0;
+    while True:
+        last_command = view.command_history(i)
+        if last_command[0] != 'vi_yank_stack':
+            break
         else:
-            raise Exception('ViYankStack: fallback command scope undefined!')
+            i = i - 1
+    return last_command
+
+class InputStateTracker(sublime_plugin.EventListener):
+    def on_query_context(self, view, key, operator, operand, match_all):
+        if key == "vi_has_just_pasted":
+            last_command = last_command_before_yank_stack(view)
+            has_just_pasted = last_command[0] in ('vi_paste_left', 'vi_paste_right')
+            # debug('last_command', last_command)
+            # debug('has_just_pasted', has_just_pasted)
+            if operator == sublime.OP_EQUAL:
+                return operand == has_just_pasted
+            if operator == sublime.OP_NOT_EQUAL:
+                return operand != has_just_pasted
+        return None
 
 class ViYankStackCommand(sublime_plugin.TextCommand):
-    def run(self, edit, forward=False, fallback_command=False,
+    def run_(self, edit_token, args):
+        if 'event' in args:
+            del args['event']
+
+        return self.run(**args)
+
+    def run(self, forward=False, fallback_command=False,
             fallback_scope = 'window', fallback_args = {}):
-        last_command = self.view.command_history(0)
+        debug('current g_registers')
+        pprint(g_registers)
+        last_command = last_command_before_yank_stack(self.view)
         if last_command[0] in ('vi_paste_left', 'vi_paste_right'):
             for i in range(9, -1, -1):
                     if '%s' % i in g_registers:
@@ -34,20 +58,22 @@ class ViYankStackCommand(sublime_plugin.TextCommand):
                         g_registers['%s' % i] = g_registers['%s' % (i - 1)]
                     g_registers['0'] = g_registers['_']
 
+                del g_registers['_']
                 g_registers['"'] = g_registers['0']
-                g_registers['+'] = g_registers['0']
-                g_registers['*'] = g_registers['0']
-                
+                debug('shifted g_registers')
+                pprint(g_registers)
                 if self.view.settings().get('vintage_use_clipboard', False):
                     sublime.set_clipboard(g_registers['0'])
+                    g_registers['+'] = g_registers['0']
+                    g_registers['*'] = g_registers['0']
             except KeyError:
                 pass
             else:
+                debug('undo')
                 self.view.window().run_command('undo')
-                self.view.run_command(last_command[0], last_command[1])
-        elif fallback_command:
-            run_fallback_command(self.view, fallback_command,
-                                 fallback_scope, fallback_args)
+                debug('last_command', last_command)
+                for _ in range(last_command[2]):
+                    self.view.run_command(*last_command[:2])
 
 # Override ViDelete series
 class ViDelete(sublime_plugin.TextCommand):
@@ -98,7 +124,6 @@ def set_register(view, register, forward):
         pass
     else:
         if register == '0':
-            debug('Zero')
             for i in range(9, 0, -1):
                 if '%s' % (i - 1) in g_registers:
                     g_registers['%s' % i] = g_registers['%s' % (i - 1)]
@@ -109,3 +134,5 @@ def set_register(view, register, forward):
             g_registers[reg] += text
         else:
             g_registers[reg] = text
+    debug('g_registers')
+    pprint(g_registers)
